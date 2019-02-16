725c3064288ffb54fd70556aa9902eb761d33f64
HyunTae EOM <hyunt0413@gmail.com>
2019-02-16 11:38:12 +0900

    Apply image picker

--- src/containers/Bottom/Text.js
+++ src/containers/Bottom/Text.js
@@ -5,23 +5,49 @@ import { connect } from 'react-redux';
 import TextComponent from '../../components/Bottom/Text';
 
 import Camera from 'react-native-camera';
+import ImagePicker from 'react-native-image-picker';
 
 export class TextContainer extends Component {
 
+
     takePhoto = () => {
-      this.camera.capture()
+      const options = {
+        title: 'Select Avatar',
+        customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+        storageOptions: {
+          skipBackup: true,
+          path: 'images',
+        },
+      };
+
+      ImagePicker.showImagePicker(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        } else {
+          const source = { uri: response.uri };
+
+          // You can also display the image using data:
+          // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+          this.setState({
+            avatarSource: source,
+          });
+        }
+      });
     }
 
     render() {
       const text = '실제 달 찍기';
 
       return (
-          <Camera ref={(cam) => {
-                        this.camera = cam;
-                  }}>
             <TextComponent text={text}
                            takePhoto={this.takePhoto}/>
-          </Camera>
       );
     }
 }
